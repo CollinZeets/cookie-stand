04f5ffecc721dc4d3c145be5169174a9c83f2604
Collin Zeets <ckzeets@gmail.com>
2021-01-24 17:48:36 -0600

    lab 06 requirements

--- js/app.js
+++ js/app.js
@@ -4,12 +4,14 @@ class Store {
   constructor(minCust, maxCust, avgSale) {
     this.data = new Array(13);
     this.total = 0;
-    this.data.forEach((element) => {
-      element =
+
+    for (let i = 0; i < this.data.length; i++) {
+      this.data[i] = Math.round(
         (Math.floor(Math.random() * (maxCust - minCust + 1)) + minCust) *
-        avgSale;
-      this.total += element;
-    });
+          avgSale
+      );
+      this.total += this.data[i];
+    }
     this.average = this.total / this.data.length;
   }
 }
@@ -22,6 +24,52 @@ let stores = {
   lima: new Store(2, 16, 4.6),
 };
 
+for (const store in stores) {
+  document
+    .getElementById("storeResults")
+    .appendChild(makeUL(store, stores[store]));
+}
+
+function makeUL(name, store) {
+  // https://stackoverflow.com/questions/11128700/create-a-ul-and-fill-it-based-on-a-passed-array
+  // Create the list element:
+  let list = document.createElement("ul");
+  let div = document.createElement("div");
+  div.appendChild(document.createTextNode(name));
+  console.log(store.data);
+  for (let i = 0; i < store.data.length; i++) {
+    // Create the list item:
+    let item = document.createElement("li");
+    let time = [
+      "6am",
+      "7am",
+      "8am",
+      "9am",
+      "10am",
+      "11am",
+      "12pm",
+      "1pm",
+      "2pm",
+      "3pm",
+      "4pm",
+      "5pm",
+      "6pm",
+    ];
+    // Set its contents:
+    item.appendChild(
+      document.createTextNode(`${time[i]}; ${store.data[i]} cookies sold `)
+    );
+
+    // Add it to the list:
+    list.appendChild(item);
+  }
+  let total = document.createElement("li");
+  total.appendChild(document.createTextNode(`total: ${store.total} `));
+  list.appendChild(total);
+  div.appendChild(list);
+  // Finally, return the constructed list:
+  return div;
+}
 /*
 Location	Min / Cust	Max / Cust	Avg Cookie / Sale
 Seattle	23	65	6.3
